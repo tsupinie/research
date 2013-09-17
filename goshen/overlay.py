65ab6efeb5b6098d9f69a2bd3d75b0bdf0e5bb06
Tim Supinie <tsupinie@gmail.com>
2013-09-17 11:42:57 -0500

    Moved pylib.c to the python_scripts directory for use with weave code.

--- goshen/overlay.py
+++ goshen/overlay.py
@@ -9,7 +9,7 @@ matplotlib.use('agg')
 import pylab
 
 def main():
-    mwr = RadarObsFile("qc/1km/vols/05XP.20090605.215719")
+    mwr = RadarObsFile("qc/1km/vols/05XP.20090605.220000")
     wsr = RadarObsFile("qc/1km/KCYS.20090605.220000")
 
     bounds = (slice(110, 135), slice(118, 143))
@@ -19,9 +19,9 @@ def main():
     
     pylab.contourf(xs, ys, wsr['vr'][0][bounds], levels=np.arange(-40, 45, 5), cmap=matplotlib.cm.get_cmap('RdBu_r'))
     pylab.colorbar()
-    pylab.contour(xs, ys, mwr['vr'][0][bounds], levels=np.arange(-40, 0, 5), linestyle='--', colors='k')
-    pylab.contour(xs, ys, mwr['vr'][0][bounds], levels=[0], linestyle='-', linewidths=1.5, colors='k')
-    pylab.contour(xs, ys, mwr['vr'][0][bounds], levels=np.arange(5, 45, 5), linestyle='-', colors='k')
+    pylab.contour(xs, ys, mwr['vr'][3][bounds], levels=np.arange(-40, 0, 5), linestyle='--', colors='k')
+    pylab.contour(xs, ys, mwr['vr'][3][bounds], levels=[0], linestyle='-', linewidths=1.5, colors='k')
+    pylab.contour(xs, ys, mwr['vr'][3][bounds], levels=np.arange(5, 45, 5), linestyle='-', colors='k')
     pylab.contour(xs, ys, mwr['Z'][0][bounds], levels=[10], linewidths=2, colors='k')
 
     grid.drawPolitical()
